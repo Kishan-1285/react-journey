6f11eabb6edde02f083bf62bad7256592124d492
Kishan-1285 <kishanragunathan26@gmail.com>
2026-02-19 16:01:41 +0530

    inline

---
+++ styles/src/Button.jsx
@@ -0,0 +1,16 @@
+function Button(){
+   const styles = {
+      
+  backgroundColor: "rgb(0, 255, 255)",
+  color: "rgb(0, 0, 0)",
+  border: "none",
+  borderRadius:"5px",
+  cursor: "pointer",
+  boxShadow: "rgb(0, 255, 255)",
+   }
+   return (
+    <button style={styles}>Touch me</button>
+   );
+}
+
+export default Button
--- styles/src/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Button.module.css'
-
-function Button(){
-   return (
-    <button className={styles.button}>Touch me</button>
-   );
-}
-
-export default Button

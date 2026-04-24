ceb8a1bf99ef4b95b20c013e0fe5d35cc19b12dd
kishan-1285 <kishanragunathan26@gmail.com>
2026-04-24 12:00:00 +0530

    Update App: Apr 24 change

---
+++ practice/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+
+function App(){
+  return(
+  <>
+    <Header/>
+  </>
+  );
+
+}
+
+// Small update: Apr 24 change
+// (automated dated commit)
+
+export default App
---
+++ practice/src/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+ return(
+    <>
+      <h1>Hello</h1>
+    </>
+ );
+
+}
+
+export default Header
